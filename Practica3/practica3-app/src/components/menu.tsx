10feb4d5dd6f56b5af7091fc61f12aa762d74712
Augusto Messina <84212395+augusMessina@users.noreply.github.com>
2023-03-17 11:11:42 +0100

    add loading screens

--- Practica3/practica3-app/src/components/menu.tsx
+++ Practica3/practica3-app/src/components/menu.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { MainMenu, PlanetsMenu } from "@/styles/myStyledComponents";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type PageProps = {
     planets: {name: string, id: string}[],
@@ -13,35 +13,73 @@ const Menu = (props: PageProps) => {
 
     const router = useRouter();
 
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleStart = (url:any) => (url !== router.asPath) && setLoading(true);
+        const handleComplete = (url:any) => (url === router.asPath) && setLoading(false);
+
+        router.events.on('routeChangeStart', handleStart)
+        router.events.on('routeChangeComplete', handleComplete)
+        router.events.on('routeChangeError', handleComplete)
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart)
+            router.events.off('routeChangeComplete', handleComplete)
+            router.events.off('routeChangeError', handleComplete)
+        }
+    })
 
     return(
         <MainMenu>
-        <PlanetsMenu> PLANETAS
+        
         {
-            props.planets.map(planet => (
+            loading ? (
                 <>
-                <Link key={planet.name} className="linkName" href={`http://localhost:3000/planet/${planet.id}`}>{planet.name}</Link>
+                    <h1>Cargando...</h1>
                 </>
-            ))
+            ) :
+            (
+                <>
+                    <PlanetsMenu> PLANETAS
+                    {
+                        props.planets.map(planet => (
+                            <>
+                            <Link key={planet.name} className="linkName" href={`http://localhost:3000/planet/${planet.id}`}>{planet.name}</Link>
+                            </>
+                        ))
+                    }
+                    </PlanetsMenu>
+                    <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
+                        {
+                            props.hasPrev && (
+                                <>
+                                    <button onClick={() => {
+                                        
+                                        router.push(`/planets/${parseInt(window.location.pathname.split("/")[2])-1}`);
+                                        
+                                        }}>ANTERIOR</button>
+                                </>
+                            )
+                        }
+                        {
+                            props.hasNext && (
+                                <>
+                                    <button onClick={() => {
+                                        
+                                        router.replace(`/planets/${parseInt(window.location.pathname.split("/")[2])+1}`);
+                                        
+                                        }}>SIGUIENTE</button>
+                                </>
+                            )
+                        }
+                    </div>
+                </>
+            )
         }
-        </PlanetsMenu>
-        <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
-            {
-                props.hasPrev && (
-                    <>
-                        <button onClick={() => {router.push(`/planets/${parseInt(window.location.pathname.split("/")[2])-1}`);}}>ANTERIOR</button>
-                    </>
-                )
-            }
-            {
-                props.hasNext && (
-                    <>
-                        <button onClick={() => {router.replace(`/planets/${parseInt(window.location.pathname.split("/")[2])+1}`);}}>SIGUIENTE</button>
-                    </>
-                )
-            }
-        </div>
+
+
+        
         </MainMenu>
     )
 }
--- Practica3/practica3-app/src/components/planetdata.tsx
+++ Practica3/practica3-app/src/components/planetdata.tsx
@@ -38,6 +38,8 @@ const PlanetData = () => {
         films: [""] 
     });
 
+    const [loading, setLoading] = useState<boolean>(false);
+
     async function fetchPlanetData() {
         const res = await fetch(`https://swapi.dev/api/planets/${window.location.pathname.split("/")[2]}/`);
         const json = await res.json();
@@ -59,40 +61,66 @@ const PlanetData = () => {
 
     useEffect(() => {fetchPlanetData()}, []);
 
+    useEffect(() => {
+        const handleStart = (url:any) => (url !== router.asPath) && setLoading(true);
+        const handleComplete = (url:any) => (url === router.asPath) && setLoading(false);
+
+        router.events.on('routeChangeStart', handleStart)
+        router.events.on('routeChangeComplete', handleComplete)
+        router.events.on('routeChangeError', handleComplete)
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart)
+            router.events.off('routeChangeComplete', handleComplete)
+            router.events.off('routeChangeError', handleComplete)
+        }
+    })
+
     return(
         <MainMenu>
-            <PlanetsMenu> INFO
-            <p>Nombre: {data.name}</p>
-            <p>Tiempo de rotación: {data.rotation_period}</p>
-            <p>Tiempo de órbita: {data.orbital_period}</p>
-            <p>Diámetro: {data.diameter}</p>
-            <p>Clima: {data.climate}</p>
-            <p>Gravedad: {data.gravity}</p>
-            <p>Terreno: {data.terrain}</p>
-            <p>Agua en superficie: {data.surface_water}</p>
-            <p>Habitantes: {data.population}</p>
-            </PlanetsMenu>
-            <PlanetsMenu>
-            RESIDENTES{
-                data.residents.map(resident => (
-                    <>
-                        <p>{resident}</p>
-                    </>
-                ))
-            }
-            </PlanetsMenu>
-            <PlanetsMenu>PELICULAS
             {
-                data.films.map(film => (
+                (data.name === "" || loading) ? (
                     <>
-                        <p>{film}</p>
+                        <h1>Cargando...</h1>
                     </>
-                ))
+                ) : (
+                    <>
+                        <PlanetsMenu> INFO
+                        <p>Nombre: {data.name}</p>
+                        <p>Tiempo de rotación: {data.rotation_period}</p>
+                        <p>Tiempo de órbita: {data.orbital_period}</p>
+                        <p>Diámetro: {data.diameter}</p>
+                        <p>Clima: {data.climate}</p>
+                        <p>Gravedad: {data.gravity}</p>
+                        <p>Terreno: {data.terrain}</p>
+                        <p>Agua en superficie: {data.surface_water}</p>
+                        <p>Habitantes: {data.population}</p>
+                        </PlanetsMenu>
+                        <PlanetsMenu>
+                        RESIDENTES{
+                            data.residents.map(resident => (
+                                <>
+                                    <p>{resident}</p>
+                                </>
+                            ))
+                        }
+                        </PlanetsMenu>
+                        <PlanetsMenu>PELICULAS
+                        {
+                            data.films.map(film => (
+                                <>
+                                    <p>{film}</p>
+                                </>
+                            ))
+                        }
+                        </PlanetsMenu>
+                        <div>
+                            <button onClick={() => {router.back()}}>ATRÁS</button>
+                        </div>
+                    </>
+                )
             }
-            </PlanetsMenu>
-            <div>
-                <button onClick={() => {router.back()}}>ATRÁS</button>
-            </div>
+            
         </MainMenu>
     )
 }
